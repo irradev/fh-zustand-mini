b374fd2806afc2eb2b37cf2726982bef137661d3
root <root@irra>
2024-03-06 11:34:01 -0600

    Using Reduxt DevTools

---
+++ src/stores/person/person.store.ts
@@ -0,0 +1,36 @@
+import { type StateCreator, create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+import { firebaseStorage } from '../storages/firebase.storage';
+// import { customSessionStorage } from '../storages/session.storage';
+
+interface PersonState {
+  firstName: string;
+  lastName: string;
+}
+
+interface Actions {
+  setFirstName: (value: string) => void;
+  setLastName: (value: string) => void;
+}
+
+const pesonStore: StateCreator<
+  PersonState & Actions,
+  [['zustand/devtools', never]]
+> = (set) => ({
+  firstName: '',
+  lastName: '',
+
+  setFirstName: (value: string) =>
+    set({ firstName: value }, false, 'setFirstName'),
+  setLastName: (value: string) =>
+    set({ lastName: value }, false, 'setLastName'),
+});
+
+export const usePersonStore = create<PersonState & Actions>()(
+  devtools(
+    persist(pesonStore, {
+      name: 'person-storage',
+      storage: firebaseStorage,
+    })
+  )
+);
---
+++ src/stores/storages/firebase.storage.ts
@@ -0,0 +1,37 @@
+import { StateStorage, createJSONStorage } from 'zustand/middleware';
+
+const firebaseUrl =
+  'https://fh-zustand-storage-default-rtdb.firebaseio.com/zustand';
+
+const storageApi: StateStorage = {
+  getItem: async function (name: string): Promise<string | null> {
+    try {
+      const data = await fetch(`${firebaseUrl}/${name}.json`).then((res) =>
+        res.json()
+      );
+      console.log(data);
+      return JSON.stringify(data);
+    } catch (error) {
+      throw Error;
+    }
+  },
+  setItem: async function (name: string, value: string): Promise<void> {
+    // console.log('setItem', { name, value });
+
+    try {
+      await fetch(`${firebaseUrl}/${name}.json`, {
+        method: 'PUT',
+        body: value,
+      }).then((res) => res.json());
+
+      return;
+    } catch (error) {
+      throw Error;
+    }
+  },
+  removeItem: function (name: string): void | Promise<void> {
+    console.log('removeItem', { name });
+  },
+};
+
+export const firebaseStorage = createJSONStorage(() => storageApi);
